4a02ae21432c584a0964dd452cd0fe179a782eca
olesy <olesyagalieva97@gmail.com>
2022-11-04 21:49:33 +0500

    Сделано Дз 4.1.

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,46 @@ public class Main {
             System.out.println(" Есть еще " + ( standing- standingUsed) + " свободных стоящих мест. ");
         }
 
+        // Задание 4
+        System.out.println(" Задание 4. ");
+        int age1 = 15;
+        if ( age1 >= 18 ) {
+            System.out.println(" Поздравляем с совершеннолетием ! ");
+        } else {
+            System.out.println(" Возраст совершеннолетия ещё не наступил, и нужно немного подождать. ");
+        }
+        // Задание 5
+        System.out.println(" Задание 5. ");
+        if ( age1 >= 7 && age1 < 18 ) {
+            System.out.println(" Ребенок ходит в школу. ");
+        } else if ( age1 >= 18 && age1 < 24 ) {
+            System.out.println(" Человек уже закончил школу и может отправляться в университет. ");
+
+        } else if ( age1 >= 24 ) {
+            System.out.println(" Человек окончил университет и ему пора искать первую работу. ");
+
+        }
+        // Задание 6.
+        System.out.println( " Задание 6. ");
+        int capacity1 = 102;
+        int seating1 = 60;
+        int standing1 = capacity1 - seating1;
+        int seatingUsed1 = 56;
+        int standingUsed1 = 30;
+        if ( seatingUsed1 < seating1 ) {
+            System.out.println(" Есть еще " + (seating1 - seatingUsed1) + " свободных сидящих мест. ");
+        } else {
+            System.out.println(" Сидячих мест нет! ");
+        }
+        if ( standingUsed1 < standing1 ) {
+            System.out.println(" Есть еще " + ( standing1- standingUsed1 ) + " свободных стоячих мест. ");
+        } else {
+            System.out.println( "Стоячих мест нет! ");
+        }
+        // Задание 7
+        System.out.println( " Задание 7. ");
+
+
 
 
 
